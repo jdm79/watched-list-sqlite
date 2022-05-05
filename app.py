5de341a6d13ff4dfc840c92f2a820a95c09e82db
jdm79 <james.malvern@gmail.com>
2022-05-05 19:11:26 +0200

    join for watched list, showing title and fix username in variable string

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ menu = """Please select one of the following options:
 3) View all movies
 4) Watch a movie
 5) View watched movies
-6) Exit
+6) Add new user
+7) Exit
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -30,14 +31,14 @@ def prompt_add_movie():
 
 def print_movie_list(heading, movies):
     print("-- {heading} --")
-    for movie in movies:
-        movie_date = datetime.datetime.fromtimestamp(movie[1])
+    for _id, title, release_date in movies:
+        movie_date = datetime.datetime.fromtimestamp(release_date)
         human_date = movie_date.strftime("%b %d %Y")
-        print(f"{movie[0]} (released on {human_date})")
+        print(f"{_id}: {title} (released on {human_date})")
     print("----- \n")
 
 def print_watched_movie_list(username, movies):
-    print("-- {username}'s watched movies --")
+    print(f"-- {username}'s watched movies --")
     for movie in movies:
         print(f"{movie[1]}")
     print("----- \n")
@@ -45,12 +46,16 @@ def print_watched_movie_list(username, movies):
 
 def prompt_watch_movie():
     username = input("Username: ")
-    movie_title = input("Enter movie title you've watched: ")
-    database.watch_movie(username, movie_title)
+    movie_id = input("Enter Movie ID: ")
+    database.watch_movie(username, movie_id)
+
+def prompt_add_user():
+    username = input("Add username: ")
+    database.add_user(username)
 
 
 # using walrus operator below to refactor code 
-while (user_input := input(menu)) != "6":
+while (user_input := input(menu)) != "7":
     if user_input == "1":
         prompt_add_movie()
         
@@ -69,6 +74,9 @@ while (user_input := input(menu)) != "6":
         username = input("Username: ")
         movies = database.get_watched_movies(username)
         print_watched_movie_list(username, movies)
+    
+    elif user_input == "6":
+        prompt_add_user()
     
     else:
         print("Invalid option - please try again")
